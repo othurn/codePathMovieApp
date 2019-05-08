7ace63035f664cf7ea26c6fd8f9e11588f843b00
Oliver <othurn@ucsd.edu>
2019-05-07 19:01:06 -0700

    first

---
+++ codepathMoviesProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  codepathMoviesProject
+//
+//  Created by Oliver Thurn on 5/7/19.
+//  Copyright © 2019 Oliver Thurn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
